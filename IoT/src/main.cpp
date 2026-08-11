1fc7fa0d7f2d061141824082d5a6fe15c57d2e20
9959258-omar <9959258@schoolsnet.act.edu.au>
2026-08-11 14:11:13 +1000

    visual code

--- IoT/src/main.cpp
+++ IoT/src/main.cpp
@@ -1,26 +1,24 @@
-#include <Arduino.h>
+// MQTT client name
+// TODO - Change the name to the specific module name.
+const char *mqttClient = "ESP32-Omar"; // This should be unique for each ESP32, e.g: "ESP32_Servo", "ESP32_Piezo", etc
 
-// Set a unique identifier for your device before importing comms.h
-const char *mqttClient = "ESP32_Ryan"; // EDIT THIS FIELD
-
+// MQTT Topic
 const char *mqttTopic;
 
+#include <Arduino.h>
 #include "comms.h"
 
 void performActionBasedOnPayload(String payload)
 {
-    Serial.print("Payload received: ");
+    Serial.print("Payload: ");
     Serial.println(payload);
-
-    // Turn built-in LED ON if payload starts with '1', else OFF
-    if (payload.length() > 0 && payload[0] == '1')
+    if ((char)payload[0] == '1')
     {
-        Serial.println("Action: LED ON");
+        Serial.println("LED ON");
         digitalWrite(LED_BUILTIN, HIGH);
     }
     else
     {
-        Serial.println("Action: LED OFF");
         digitalWrite(LED_BUILTIN, LOW);
     }
 }
@@ -29,31 +27,26 @@ void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
-    
     wifiSetup();
     mqttSetup();
-
     while (!Serial)
     {
         delay(10);
     }
     delay(1000);
+
+    randomSeed(analogRead(A0));   // Seed using an unconnected analog pin for real randomness
 }
 
 void loop()
 {
-    // 1. Maintain connection to the broker
-    mqttConnect();
+    // 1. Handle Connection Persistence
+    mqttConnect(); // Ensure we are connected to the MQTT broker. If not, this will attempt to reconnect.
 
-    // 2. Transmit periodic telemetry (if required by design specification)
-    unsigned long now = millis();
-    if (now - lastUpdate > updateInterval)
-    {
-        lastUpdate = now;
-        // TODO: Insert customized sendDataToServer() calls here.
-    }
+    // 2. Generate and send a random number periodically
+     int randomNumber = random(1, 100001); 
+    sendPeriodicUpdate("sensorData", String(randomNumber));    
 
-    // 3. Yield execution time for PubSubClient processing
-    client.loop();
+    client.loop(); // Check for incoming messages and keep the connection alive
     delay(100);
 }
